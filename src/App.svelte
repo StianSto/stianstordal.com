b2fbb7d5bead39f38a5927fe723dce9d30937576
Stian Stordal <stordalstian@gmail.com>
2023-06-10 05:10:23 +0200

    added hero section

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,22 @@
 <script>
   import Button from "./lib/components/Button.svelte";
   import ToggleSwitch from "./lib/components/ToggleSwitch.svelte";
+  import Hero from "./sections/Hero.svelte";
+  import sectionDividerHero from "./assets/section-divider-hero.svg";
 </script>
 
 <main>
-  <section>
-    <ToggleSwitch />
-  </section>
+  <Hero />
+  <img src={sectionDividerHero} alt="" id="sectionDividerhero" />
 </main>
 
 <style>
   main {
     margin-inline: auto;
+  }
+
+  #sectionDividerhero {
+    width: 100%;
+    translate: 0 -60%;
   }
 </style>
