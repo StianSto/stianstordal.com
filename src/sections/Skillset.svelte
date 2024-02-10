7ba798858c7da1993f95cf113d44d54bc3722df4
Stian Stordal <stordalstian@gmail.com>
2024-02-10 08:30:24 +0100

    add in-transitions skillset

--- src/sections/Skillset.svelte
+++ src/sections/Skillset.svelte
@@ -1,11 +1,31 @@
 <script>
+  import { fly } from "svelte/transition";
   import Skillset from "./Skillset.svelte";
   import cypressLogo from "../assets/cypress-logo.svg";
   import skillsetTopBow from "../assets/skillsetTopBow.svg";
   import { lang } from "../lib/stores";
+  import { onDestroy, onMount } from "svelte";
 
   let currentLang;
   lang.subscribe((data) => (currentLang = data));
+
+  let visible = false;
+  let element;
+
+  function handleScroll() {
+    const bounding = element.getBoundingClientRect();
+
+    if (bounding.top <= document.documentElement.clientHeight) {
+      visible = true;
+    }
+  }
+  onMount(() => {
+    window.addEventListener("scroll", handleScroll);
+  });
+
+  onDestroy(() => {
+    window.removeEventListener("scroll", handleScroll);
+  });
 </script>
 
 <section id="skillset">
@@ -23,101 +43,105 @@
     />
   </svg>
 
-  <div class="container">
-    <h2>
-      {#if currentLang === "en"}
-        My Skillset
-      {:else}
-        Min Kompetanse
-      {/if}
-    </h2>
-    <div>
-      <div class="skills-frontend skills-container">
-        <h3>FrontEnd</h3>
-        <ul>
-          <li>
-            <i class="devicon-html5-plain" />
-            <span>HTML</span>
-          </li>
-          <li>
-            <i class="devicon-css3-plain" />
-            <span>CSS</span>
-          </li>
-          <li>
-            <i class="devicon-javascript-plain" />
-            <span>JavaScript</span>
-          </li>
-          <li>
-            <i class="devicon-typescript-plain" />
-            <span>TypeScript</span>
-          </li>
-          <li>
-            <i class="devicon-bootstrap-plain" />
-            <span>Bootstrap</span>
-          </li>
-          <li>
-            <i class="devicon-tailwindcss-plain" />
-
-            <span>Tailwind</span>
-          </li>
-          <li>
-            <i class="devicon-sass-plain" />
-            <span>Sass</span>
-          </li>
-          <li>
-            <i class="devicon-react-plain" />
-            <span>React</span>
-          </li>
-          <li>
-            <i class="devicon-svelte-plain" />
-            <span>Svelte</span>
-          </li>
-          <li class="no-icon">
-            <img class="cypress" src={cypressLogo} alt="cypress logo" />
-            <span>Cypress</span>
-          </li>
-          <li>
-            <i class="devicon-jest-plain" />
-            <span>Jest</span>
-          </li>
-        </ul>
+  <div class="container" bind:this={element}>
+    {#key visible}
+      <h2>
+        {#if currentLang === "en"}
+          My Skillset
+        {:else}
+          Min Kompetanse
+        {/if}
+      </h2>
+      <div>
+        <div class="skills-frontend skills-container">
+          <h3>FrontEnd</h3>
+          <ul>
+            <li in:fly={{ x: "100%", duration: 500, delay: 50 }} class:visible>
+              <i class="devicon-html5-plain" />
+              <span>HTML</span>
+            </li>
+            <li in:fly={{ x: "100%", duration: 500, delay: 100 }} class:visible>
+              <i class="devicon-css3-plain" />
+              <span>CSS</span>
+            </li>
+            <li in:fly={{ x: "100%", duration: 500, delay: 150 }} class:visible>
+              <i class="devicon-javascript-plain" />
+              <span>JavaScript</span>
+            </li>
+            <li in:fly={{ x: "100%", duration: 500, delay: 200 }} class:visible>
+              <i class="devicon-typescript-plain" />
+              <span>TypeScript</span>
+            </li>
+            <li in:fly={{ x: "100%", duration: 500, delay: 250 }} class:visible>
+              <i class="devicon-bootstrap-plain" />
+              <span>Bootstrap</span>
+            </li>
+            <li in:fly={{ x: "100%", duration: 500, delay: 300 }} class:visible>
+              <i class="devicon-tailwindcss-plain" />
+              <span>Tailwind</span>
+            </li>
+            <li in:fly={{ x: "100%", duration: 500, delay: 350 }} class:visible>
+              <i class="devicon-sass-plain" />
+              <span>Sass</span>
+            </li>
+            <li in:fly={{ x: "100%", duration: 500, delay: 400 }} class:visible>
+              <i class="devicon-react-plain" />
+              <span>React</span>
+            </li>
+            <li in:fly={{ x: "100%", duration: 500, delay: 450 }} class:visible>
+              <i class="devicon-svelte-plain" />
+              <span>Svelte</span>
+            </li>
+            <li
+              in:fly={{ x: "100%", duration: 500, delay: 500 }}
+              class:visible
+              class="no-icon"
+            >
+              <img class="cypress" src={cypressLogo} alt="cypress logo" />
+              <span>Cypress</span>
+            </li>
+            <li in:fly={{ x: "100%", duration: 500, delay: 550 }} class:visible>
+              <i class="devicon-jest-plain" />
+              <span>Jest</span>
+            </li>
+          </ul>
+        </div>
+        <div class="skills-backend skills-container">
+          <h3>BackEnd</h3>
+          <ul>
+            <li in:fly={{ x: "100%", duration: 500, delay: 600 }} class:visible>
+              <i class="devicon-express-original" />
+              <span>Express</span>
+            </li>
+            <li in:fly={{ x: "100%", duration: 500, delay: 650 }} class:visible>
+              <i class="devicon-mongodb-plain" />
+              <span>MongoDB</span>
+            </li>
+          </ul>
+        </div>
+        <div class="skills-design skills-container">
+          <h3>Design</h3>
+          <ul>
+            <li in:fly={{ x: "100%", duration: 500, delay: 700 }} class:visible>
+              <i class="devicon-xd-plain" />
+              <span>Adobe XD</span>
+            </li>
+            <li in:fly={{ x: "100%", duration: 500, delay: 750 }} class:visible>
+              <i class="devicon-illustrator-plain" />
+              <span>Adobe Illustrator</span>
+            </li>
+            <li in:fly={{ x: "100%", duration: 500, delay: 800 }} class:visible>
+              <i class="devicon-photoshop-plain" />
+              <span>Adobe Photoshop</span>
+            </li>
+            <li in:fly={{ x: "100%", duration: 500, delay: 850 }} class:visible>
+              <i class="devicon-figma-plain" />
+              <span>Figma</span>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="skills-backend skills-container">
-        <h3>BackEnd</h3>
-        <ul>
-          <li>
-            <i class="devicon-express-original" />
-            <span>Express</span>
-          </li>
-          <li>
-            <i class="devicon-mongodb-plain" />
-
-            <span>MongoDB</span>
-          </li>
-        </ul>
-      </div>
-      <div class="skills-design skills-container">
-        <h3>Design</h3>
-        <ul>
-          <li>
-            <i class="devicon-xd-plain" />
-            <span>Adobe XD</span>
-          </li>
-          <li>
-            <i class="devicon-illustrator-plain" />
-            <span>Adobe Illustrator</span>
-          </li>
-          <li>
-            <i class="devicon-photoshop-plain" />
-            <span>Adobe Photoshop</span>
-          </li>
-          <li>
-            <i class="devicon-figma-plain" />
-            <span>Figma</span>
-          </li>
-        </ul>
-      </div>
-    </div>
+    {/key}
   </div>
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -180,6 +204,11 @@
         display: flex;
         justify-content: center;
         align-items: center;
+        opacity: 0;
+
+        &.visible {
+          opacity: 1;
+        }
 
         & span {
           width: fit-content;
